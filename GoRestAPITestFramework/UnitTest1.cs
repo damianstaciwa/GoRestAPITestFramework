16e32b4aa0c3e0980e8b1ecf80e4d14726b78fb6
dstaciwa <damian_staciwa@outlook.com>
2023-04-04 13:31:00 +0200

    Initial commit

---
+++ GoRestAPITestFramework/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GoRestAPITestFramework
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
