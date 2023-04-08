9e2141f0e485d96e46525a7f305273534acc3f2a
dstaciwa <damian_staciwa@outlook.com>
2023-04-08 21:28:53 +0200

    UserPostsTest.cs added with GET nad POST tests

---
+++ GoRestAPITestFramework/Tests/UserPostsTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using GoRestAPITestFramework.Models;
+using NUnit.Framework;
+
+namespace GoRestAPITestFramework
+{
+    [TestFixture]
+    public class UserPostsTests
+    {
+        private readonly RestApiClient apiClient;
+
+        public UserPostsTests()
+        {
+            apiClient = new RestApiClient();
+        }
+
+        [Test]
+        public async Task FullUserPostsCRUDTest()
+        {
+            var emailPrefix = DateTime.Now.ToString("ddMMyyyyHHmmss");
+
+            var user = new User
+            {
+                Name = "Test User",
+                Email = $"{emailPrefix}@gmail.com",
+                Gender = "Male",
+                Status = "active"
+            };
+
+            await apiClient.Users.CreateUserAsync(user);
+            var users = await apiClient.Users.GetUsersAsync();
+
+            var createdUser = users.Find(u => u.Email == user.Email);
+            createdUser.Should().NotBeNull();
+
+            int userId = createdUser.Id;
+
+            var post = new Post
+            {
+                Title = $"Test Post from {user.Name}",
+                Body = "This is a test post for user posts test."
+            };
+
+            await apiClient.UserPosts.CreateUserPostAsync(userId, post);
+
+            var userPosts = await apiClient.UserPosts.GetUserPostsAsync(userId);
+            userPosts.Should().NotBeNullOrEmpty();
+
+            var createdPost = userPosts.Find(p => p.Title == post.Title);
+            createdPost.Should().NotBeNull();
+            createdPost.User_Id.Should().Be(userId);
+            createdPost.Body.Should().BeEquivalentTo(post.Body);
+        }
+    }
+}
