4fbbc2ec7d083bbbcc42bd338d6a6c44c8928fdb
dstaciwa <damian_staciwa@outlook.com>
2023-04-10 21:13:37 +0200

    TodoItem model modified

--- GoRestAPITestFramework/Models/TodoItem.cs
+++ GoRestAPITestFramework/Models/TodoItem.cs
@@ -3,7 +3,7 @@
     public class TodoItem
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
+        public int User_Id { get; set; }
         public string? Title { get; set; }
         public string? DueOn { get; set; }
         public string? Status { get; set; }
